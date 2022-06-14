e6adfd7cd133d48331427daecf9da876351ea785
Oryange Strifezze <oryange.strifezze@mercadolivre.com>
2022-06-14 10:08:35 -0300

    Add projeto integrador

---
+++ Aula 5 -  Introdução Android/app/src/main/java/com/example/project/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.example.project
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.TextView
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val textView = findViewById<TextView>(R.id.hello_world)
+        textView.text = getString(R.string.hello_world_text)
+    }
+}
